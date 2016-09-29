8180f3ad548868bf664eaa493a7bfcd5746d0911
BradyHammond <bradymh23@gmail.com>
2016-09-28 22:17:44 -0600

    Update database.php

--- database.php
+++ database.php
@@ -3,24 +3,24 @@
 ===================================================-->
 
 <?php
-	$server = 'XXX.XX.XX.XXX';
-	$username = "XXXXX";
-	$password = "XXXXX";
-	$database_I = "XXXXX";
-	$database_II = "XXXXX";
-	
-	session_start();
-	
-	try
-	{
-		$connection_I = new PDO("mysql:host=$server;dbname=$database_I;", $username, $password);
-		$connection_II = new PDO("mysql:host=$server;dbname=$database_II;", $username, $password);
-	}
-	
-	catch(PDOException $e)
-	{
-		die("Connection Failed:  " . $e->getMessage());
-	}	
+  $server = 'XXX.XX.XX.XXX';
+  $username = "XXXXX";
+  $password = "XXXXX";
+  $database_I = "XXXXX";
+  $database_II = "XXXXX";
+
+  session_start();
+
+  try
+  {
+    $connection_I = new PDO("mysql:host=$server;dbname=$database_I;", $username, $password);
+    $connection_II = new PDO("mysql:host=$server;dbname=$database_II;", $username, $password);
+  }
+
+  catch(PDOException $e)
+  {
+    die("Connection Failed:  " . $e->getMessage());
+  }	
 ?>
 
 <!--==================================================-->
